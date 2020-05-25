871ed3836474de877dd9dc6467a50bfb12f226e8
Ytt-yusuke <43527062+Ytt-yusuke@users.noreply.github.com>
2020-05-25 14:46:07 +0900

    Update board.h

--- src/board.h
+++ src/board.h
@@ -1,6 +1,38 @@
-﻿#pragma once
+#pragma once
 #include <iostream>
+#include <vector>
 
+class Point {
+	int x_ = -1,
+		y_ = -1;
+public:
+	Point() {};
+	Point(int x, int y) :x_(x), y_(y) {}
+	int x() const { return x_; }
+	int y() const { return y_; }
+	void setX(int x) { x_ = x; }
+	void setY(int y) { y_ = y; }
+	void set(int x, int y) { x_ = x; y_ = y; }
+
+	bool operator == (const Point& p) const {
+		return p.x() == x_ && p.y() == y_;
+	}
+	bool operator != (const Point& p) const {
+		return !(p == *this);
+	}
+
+	Point getRight() const { return Point(x_ + 1, y_); }
+	Point getLeft() const { return Point(x_ - 1, y_); }
+	Point getUp() const { return Point(x_, y_ + 1); }
+	Point getDown() const { return Point(x_, y_ - 1); }
+
+	static double distance(const Point p1, const Point p2)
+	{
+		double dx = (double)p2.x() - (double)p1.x();
+		double dy = (double)p2.y() - (double)p1.y();
+		return sqrt(dx * dx + dy * dy);
+	}
+};
 
 class Mass {
 public:
@@ -13,27 +45,43 @@ public:
 		WATER,// 進むのが1/3に遅くなる
 		ROAD,//進むのが3倍速い
 	};
+	enum listed
+	{
+		NONE,
+		OPEN,
+		CLOSE,
+	};
 private:
 	status s_ = BLANK;
+	listed listed_ = NONE;
+	Point pos_;
+	Mass* pParent_ = nullptr;
+	int steps_ = 0;
+	double estimate_ = 0.0;
+
+	static int getWalkCost(Mass& m) { status s = m.getStatus(); return (s == WATER) ? 3 : (s == ROAD) ? 1/3 : 1; }
+
+	void calcCost(const Point target)
+	{
+		steps_ = (pParent_ ? pParent_->steps_ : 0) + getWalkCost(*this);
+		estimate_ = Point::distance(pos_,  target);
+	}
 public:
 	void setStatus(status s) { s_ = s; }
 	status getStatus() const { return s_; }
-};
 
-class Point {
-	int x_ = -1,
-		y_ = -1;
-public:
-	Point(int x, int y) :x_(x), y_(y) {}
-	int x() const { return x_; }
-	int y() const { return y_; }
-	void setX(int x) { x_ = x; }
-	void setY(int y) { y_ = y; }
+	void setPos(int x, int y) { pos_.set(x, y); }
+	const Point& getPos() const { return pos_; }
+	int x() { return pos_.x(); }
+	int y() { return pos_.y(); }
 
-	bool operator == (const Point& p) const {
-		return p.x() == x_ && p.y() == y_;}
-	bool operator != (const Point& p) const {
-		return !(p == *this);}
+	void setParent(Mass* pParent, const Point& goal) { pParent_ = pParent; calcCost(goal); }
+	Mass* getParent() { return pParent_; }
+
+	void setListed(listed t) { listed_ = t; }
+	bool isListed(listed t) const { return listed_ == t; }
+
+	double getCost() const { return (double)steps_ + estimate_; }
 };
 
 class Board {
@@ -42,13 +90,16 @@ private:
 		BOARD_SIZE = 10,
 	};
 	Mass mass_[BOARD_SIZE][BOARD_SIZE];
+	Mass& getMass(const Point p) { return mass_[p.y()][p.x()]; }
+
+	std::vector<Mass*> open_list_;
 public:
+
 	Board() {
-		for (int y = 0; y < BOARD_SIZE; y++) {
-			for (int x = 0; x < BOARD_SIZE; x++) {
-				mass_[y][x].setStatus(Mass::BLANK);
-			}
-		}
+		for (int y = 0; y < BOARD_SIZE; y++)
+			for (int x = 0; x < BOARD_SIZE; x++)
+				mass_[y][x].setPos(x, y);
+
 		// 壁
 		mass_[4][6].setStatus(Mass::WALL);
 		mass_[4][5].setStatus(Mass::WALL);
@@ -69,7 +120,15 @@ public:
 		mass_[6][2].setStatus(Mass::ROAD);
 		mass_[6][3].setStatus(Mass::ROAD);
 	}
-	~Board() {}
+
+	bool isValidated(const Point& p)
+	{
+		if (getMass(p).getStatus() == Mass::WALL)
+		{
+			return false;
+		}
+		return true;
+	}
 
 	bool find(const Point& start, const Point& goal);
 
